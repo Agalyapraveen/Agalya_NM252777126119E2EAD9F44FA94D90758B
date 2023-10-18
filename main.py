d217b24ceacc5dd9c4909cb73c857fdbfc58d114
Agalyapraveen <148317387+Agalyapraveen@users.noreply.github.com>
2023-10-18 12:48:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#1.1 implement a recursive function to calculate the factorial of given number
+def recur_factorial(n):
+  if n==1:
+    return n
+  else:
+    return n*recur_factorial(n-1)
+    # take input from the user
+num=int(input ( "Enter a number: "))
+    # check is the number is negative
+if num<0:
+  print("sorry, factorial does not exist for negative numbers")
+elif num==0:
+  print ( "The factorial of 0 is 1")
+else:
+  
+  print ( "The factorial of", num, "is", recur_factorial(num))
